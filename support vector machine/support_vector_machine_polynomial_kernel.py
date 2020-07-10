8bdec04db431d6427f5c91e7b2730da05f7bab1f
raniaarinta <64395618+raniaarinta@users.noreply.github.com>
2020-07-10 14:25:33 +0700

    Add files via upload

---
+++ support vector machine/support_vector_machine_polynomial_kernel.py
@@ -0,0 +1,23 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+from sklearn import  metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+cancer = datasets.load_breast_cancer()
+
+
+X = cancer.data
+Y = cancer.target
+
+#split data
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2)
+
+classes =['malignant' 'benign']
+
+#SVM classification
+clf = svm.SVC(kernel="poly", degree=5)
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+acc = metrics.accuracy_score(y_test,y_pred)
+print(acc)
