36b5f9bcbc2a86751824d48eefab88bba91e227e
raniaarinta <64395618+raniaarinta@users.noreply.github.com>
2020-07-20 11:07:08 +0700

    Add files via upload

---
+++ Confusion Matrix, Accuracy, Precision and Recall/suppor_vector_machine_linear-kernel.py
@@ -0,0 +1,40 @@
+import sklearn
+from sklearn import datasets
+from sklearn import svm
+from sklearn import metrics
+from sklearn.metrics import classification_report as  cr
+from sklearn.metrics import precision_recall_curve as prc
+
+
+cancer = datasets.load_breast_cancer()
+
+
+X = cancer.data
+Y = cancer.target
+
+#split data
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.2)
+
+classes =['malignant' 'benign']
+
+#SVM classification
+clf = svm.SVC(kernel="linear", C=2)
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+#print accuracy
+acc = metrics.accuracy_score(y_test,y_pred)
+print("accuracy: \n")
+print(acc)
+#print confusion matrix
+confusion_matrix=metrics.confusion_matrix(y_test,y_pred)
+print("confusion matrix: \n")
+print(confusion_matrix)
+#classification_report
+classification_report=cr(y_test,y_pred)
+print(classification_report)
+#precission recall
+Precision,Recall, threshold= prc(y_test,y_pred)
+print("Precision: ",Precision)
+print("Recall: ",Recall)
+print("threshold: ",threshold)
+
